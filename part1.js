b587108218340927db133a638beca60e6d47019b
Chetan Naik <chetannaik11145@gmail.com>
2024-09-05 20:56:17 +0530

    added ,y javascript first program
    done

---
+++ part1.js
@@ -0,0 +1,32 @@
+let a = 25;
+let b = 25;
+
+let myName = "Chetan Naik";
+
+console.log(myName[0]);
+console.log(myName[1]);
+console.log(myName[2]);
+console.log(myName[3]);
+console.log(myName[4]);
+console.log(myName[5]);
+console.log(myName[6]);
+console.log(myName[7]);
+console.log(myName[8]);
+console.log(myName[9]);
+console.log(myName[10]);
+console.log(myName[11]);
+
+//String Concatination
+
+let firstName = "Chetan";
+let lastName = "Naik";
+
+console.log(firstName + " " + lastName);
+
+
+console.log(myName[myName.length-1]); //This will print last letter of my name of the string
+
+let num1 = 10;
+let num2 = 30;
+
+console.log(num1 + num2);
